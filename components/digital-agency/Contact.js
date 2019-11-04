1af2836956ccee8f655621865664165426f5b7b7
Jaydn Chou <jaydn.chou@gmail.com>
2019-11-04 21:05:03 +1100

    Refactored from class to functional component

--- components/digital-agency/Contact.js
+++ components/digital-agency/Contact.js
@@ -3,34 +3,30 @@ import React, { Component } from "react";
 import "./Contact.css";
 
 const EMAIL = "info@deadlyconnections.org.au";
-export class Contact extends Component {
-  render() {
-    return (
-      <section className="contact-cta-area ptb-120">
-        <div className="container">
-          <div className="contact-cta-content">
-            <div className="section-title">
-              <span>Get in Touch</span>
-              <h2>We're here to help</h2>
-            </div>
 
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
-              ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
-              accumsan lacus vel facilisis.
-            </p>
-            <a
-              className="contact-btn"
-              href={`mailto:${EMAIL}?Subject=Website enquiry`}
-            >
-              Contact Us
-            </a>
-          </div>
+const Contact = () => (
+  <section className="contact-cta-area ptb-120">
+    <div className="container">
+      <div className="contact-cta-content">
+        <div className="section-title">
+          <span>Get in Touch</span>
+          <h2>We're here to help</h2>
         </div>
-      </section>
-    );
-  }
-}
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis.
+        </p>
+        <a
+          className="contact-btn"
+          href={`mailto:${EMAIL}?Subject=Website enquiry`}
+        >
+          Contact Us
+        </a>
+      </div>
+    </div>
+  </section>
+);
 
 export default Contact;
