ea0b352b56467ff0a2cedeb7b5a6d2258718e70b
Jaydn Chou <jaydn.chou@gmail.com>
2019-11-05 10:30:33 +1100

    Refactored from class to functional component

--- components/digital-agency/Team.js
+++ components/digital-agency/Team.js
@@ -1,142 +1,175 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./Team.css";
 
-export class Team extends Component {
-  render() {
-    return (
-      <section className="team-area bg-image ptb-120">
-        <div className="container">
-          <div className="section-title">
-            <span>Our Events</span>
-            <h2>Reach out and relate</h2>
+// Todo: Refactor Team component to use EventCard to DRY up code
+const EventCard = () => {
+  return (
+    <div className="col-lg-4 col-md-6 col-sm-6">
+      <div className="single-team">
+        <div className="team-image">
+          <img
+            src={require("../../images/team-image/1.jpg")}
+            className="member-img"
+            alt="team"
+          />
+          <img
+            src={require("../../images/team-image/team-back.jpg")}
+            className="back-image"
+            alt="back-image"
+          />
+          <ul className="social-links">
+            <li>
+              <a href="#">
+                <i className="icofont-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="icofont-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="icofont-instagram"></i>
+              </a>
+            </li>
+          </ul>
+          <div className="event-content">
+            <h3>Some Event</h3>
+            <span>Sep 2019</span>
           </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-          <div className="row">
-            <div className="col-lg-4 col-md-6 col-sm-6">
-              <div className="single-team">
-                <div className="team-image">
-                  <img
-                    src={require("../../images/team-image/1.jpg")}
-                    className="member-img"
-                    alt="team"
-                  />
-                  <img
-                    src={require("../../images/team-image/team-back.jpg")}
-                    className="back-image"
-                    alt="back-image"
-                  />
+const Team = () => (
+  <section className="team-area bg-image ptb-120">
+    <div className="container">
+      <div className="section-title">
+        <span>Our Events</span>
+        <h2>Reach out and relate</h2>
+      </div>
 
-                  <ul className="social-links social-icons">
-                    <li>
-                      <a href="#">
-                        <i className="icofont-facebook"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="icofont-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="icofont-instagram"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="event-content">
-                  <h3>Some Event</h3>
-                  <span>Sep 2019</span>
-                </div>
-              </div>
+      <div className="row">
+        <div className="col-lg-4 col-md-6 col-sm-6">
+          <div className="single-team">
+            <div className="team-image">
+              <img
+                src={require("../../images/team-image/1.jpg")}
+                className="member-img"
+                alt="team"
+              />
+              <img
+                src={require("../../images/team-image/team-back.jpg")}
+                className="back-image"
+                alt="back-image"
+              />
+              <ul className="social-links social-icons">
+                <li>
+                  <a href="#">
+                    <i className="icofont-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="icofont-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="icofont-instagram"></i>
+                  </a>
+                </li>
+              </ul>
             </div>
-
-            <div className="col-lg-4 col-md-6 col-sm-6">
-              <div className="single-team">
-                <div className="team-image">
-                  <img
-                    src={require("../../images/team-image/2.jpg")}
-                    className="member-img"
-                    alt="team"
-                  />
-                  <img
-                    src={require("../../images/team-image/team-back.jpg")}
-                    className="back-image"
-                    alt="back-image"
-                  />
-
-                  <ul className="social-links">
-                    <li>
-                      <a href="#">
-                        <i className="icofont-facebook"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="icofont-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="icofont-instagram"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="event-content">
-                  <h3>Some Event</h3>
-                  <span>Oct 2019</span>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6 col-sm-6">
-              <div className="single-team">
-                <div className="team-image">
-                  <img
-                    src={require("../../images/team-image/3.jpg")}
-                    className="member-img"
-                    alt="team"
-                  />
-                  <img
-                    src={require("../../images/team-image/team-back.jpg")}
-                    className="back-image"
-                    alt="back-image"
-                  />
-
-                  <ul className="social-links">
-                    <li>
-                      <a href="#">
-                        <i className="icofont-facebook"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="icofont-twitter"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="icofont-instagram"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="event-content">
-                  <h3>Some Event</h3>
-                  <span>Coming soon</span>
-                </div>
-              </div>
+            <div className="event-content">
+              <h3>Some Event</h3>
+              <span>Sep 2019</span>
             </div>
           </div>
         </div>
-      </section>
-    );
-  }
-}
+
+        <div className="col-lg-4 col-md-6 col-sm-6">
+          <div className="single-team">
+            <div className="team-image">
+              <img
+                src={require("../../images/team-image/2.jpg")}
+                className="member-img"
+                alt="team"
+              />
+              <img
+                src={require("../../images/team-image/team-back.jpg")}
+                className="back-image"
+                alt="back-image"
+              />
+              <ul className="social-links">
+                <li>
+                  <a href="#">
+                    <i className="icofont-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="icofont-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="icofont-instagram"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="event-content">
+              <h3>Some Event</h3>
+              <span>Oct 2019</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-lg-4 col-md-6 col-sm-6">
+          <div className="single-team">
+            <div className="team-image">
+              <img
+                src={require("../../images/team-image/3.jpg")}
+                className="member-img"
+                alt="team"
+              />
+              <img
+                src={require("../../images/team-image/team-back.jpg")}
+                className="back-image"
+                alt="back-image"
+              />
+              <ul className="social-links">
+                <li>
+                  <a href="#">
+                    <i className="icofont-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="icofont-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i className="icofont-instagram"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="event-content">
+              <h3>Some Event</h3>
+              <span>Coming soon</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
 
 export default Team;
